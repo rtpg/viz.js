be8acd69e78ed8ab3dea32433555467cf107b7ae
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-01-26 23:04:41 +0100

    Add tests on input

--- test/browser.js
+++ test/browser.js
@@ -6,6 +6,25 @@ args[args.findIndex(flag => /enable-features/.test(flag))] +=
   ",ExperimentalProductivityFeatures,ImportMaps";
 const PUPPETEER_OPTIONS = { args };
 
+const makeVizGloballyAvailableOn = page =>
+  page.evaluate(function() {
+    window.getViz = () =>
+      Promise.all([
+        import("@aduh95/viz.js").then(m => m.default),
+        import("@aduh95/viz.js/worker").then(m => m.default),
+      ]).then(([Viz, workerURL]) => new Viz({ workerURL }));
+    window.domParser = new DOMParser();
+    window.parseSVG = svg =>
+      window.domParser.parseFromString(svg, "image/svg+xml");
+    window.checkSVG = svg => {
+      try {
+        return window.parseSVG(svg) instanceof SVGDocument;
+      } catch {
+        return false;
+      }
+    };
+  });
+
 describe("Test graph rendering using web browser", function() {
   /**
    * @type {{close:Promise<void>, port: Promise<number>}}
@@ -40,23 +59,8 @@ describe("Test graph rendering using web browser", function() {
     ];
 
     await page.goto(`http://localhost:${await server.port}/`);
-    await page.evaluate(function getViz() {
-      window.getViz = () =>
-        Promise.all([
-          import("@aduh95/viz.js").then(m => m.default),
-          import("@aduh95/viz.js/worker").then(m => m.default),
-        ]).then(([Viz, workerURL]) => new Viz({ workerURL }));
-      window.domParser = new DOMParser();
-      window.parseSVG = svg =>
-        window.domParser.parseFromString(svg, "image/svg+xml");
-      window.checkSVG = svg => {
-        try {
-          return window.parseSVG(svg) instanceof SVGDocument;
-        } catch {
-          return false;
-        }
-      };
-    });
+    await makeVizGloballyAvailableOn(page);
+
     for (const graph of graphs) {
       await assert.doesNotReject(
         page.evaluate(
@@ -68,5 +72,142 @@ describe("Test graph rendering using web browser", function() {
         )
       );
     }
+
+    await page.close();
+  });
+
+  it("result from first graph in input is returned for multiple invocations", async function() {
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${await server.port}/`);
+    await makeVizGloballyAvailableOn(page);
+
+    const viz = await page.evaluateHandle(() => window.getViz());
+
+    const resultAThenB = await page.evaluate(
+      viz => viz.renderString("digraph A {} digraph B {}", { format: "xdot" }),
+      viz
+    );
+    assert.match(
+      resultAThenB,
+      /digraph A/,
+      'Result should contain "digraph A"'
+    );
+    assert.doesNotMatch(
+      resultAThenB,
+      /digraph B/,
+      'Result should not contain "digraph B"'
+    );
+
+    const resultBThenA = await page.evaluate(
+      viz => viz.renderString("digraph B {} digraph A {}", { format: "xdot" }),
+      viz
+    );
+    assert.doesNotMatch(
+      resultBThenA,
+      /digraph A/,
+      'Result should not contain "digraph A"'
+    );
+    assert.match(
+      resultBThenA,
+      /digraph B/,
+      'Result should contain "digraph B"'
+    );
+  });
+
+  it("syntax error in graph throws exception", async function() {
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${await server.port}/`);
+    await makeVizGloballyAvailableOn(page);
+
+    await assert.rejects(
+      page.evaluate(() =>
+        getViz().then(viz => viz.renderString("digraph { \n ->"))
+      ),
+      /error in line 2 near \'->\'/
+    );
+  });
+
+  it("after throwing an exception on invalid input with an incomplete quoted string, continue to throw exceptions on valid input", async function() {
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${await server.port}/`);
+    await makeVizGloballyAvailableOn(page);
+
+    const viz = await page.evaluateHandle(() => window.getViz());
+
+    await assert.rejects(
+      page.evaluate(
+        viz => viz.renderString('digraph {\n a -> b [label="erroneous]\n}'),
+        viz
+      )
+    );
+
+    await assert.rejects(
+      page.evaluate(
+        viz => viz.renderString('digraph {\n a -> b [label="correcteous"]\n}'),
+        viz
+      )
+    );
+  });
+
+  it("syntax error following graph throws exception", async function() {
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${await server.port}/`);
+    await makeVizGloballyAvailableOn(page);
+
+    await assert.rejects(
+      page.evaluate(() =>
+        getViz().then(viz => viz.renderString("digraph { \n } ->"))
+      ),
+      /error in line 1 near \'->\'/
+    );
+  });
+
+  it("syntax error message has correct line numbers for multiple invocations", async function() {
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${await server.port}/`);
+    await makeVizGloballyAvailableOn(page);
+
+    const viz = await page.evaluateHandle(() => window.getViz());
+
+    await assert.rejects(
+      page.evaluate(viz => viz.renderString("digraph { \n } ->"), viz),
+      /error in line 1 near \'->\'/
+    );
+
+    await assert.rejects(
+      page.evaluate(viz => viz.renderString("digraph { \n } ->"), viz),
+      /error in line 1 near \'->\'/
+    );
+  });
+
+  it("input with characters outside of basic latin should not throw an error", async function() {
+    const page = await browser.newPage();
+
+    await page.goto(`http://localhost:${await server.port}/`);
+    await makeVizGloballyAvailableOn(page);
+
+    await Promise.all([
+      page
+        .evaluate(() =>
+          getViz().then(viz => viz.renderString("digraph { α -> β; }"))
+        )
+        .then(result => {
+          assert.match(result, /α/, 'Result should contain "α"');
+          assert.match(result, /β/, 'Result should contain "β"');
+        }),
+
+      page
+        .evaluate(() =>
+          getViz().then(viz => viz.renderString('digraph { a [label="åäö"]; }'))
+        )
+        .then(result =>
+          assert.match(result, /åäö/, 'Result should contain "åäö"')
+        ),
+    ]);
   });
 });
